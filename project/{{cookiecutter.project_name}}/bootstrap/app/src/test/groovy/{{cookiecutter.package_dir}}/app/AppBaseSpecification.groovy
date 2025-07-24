92a4edb8f771503bfde6c4340c0e14f1c70e8e91
tzatzosm <marsel.tzatzos@gmail.com>
2025-07-24 23:23:39 +0300

    ✨ feat(test): add AppBaseSpecification for integration testing with PostgreSQL container
    - introduce AppBaseSpecification using Spock for Spring Boot integration tests
    - configure dynamic PostgreSQL container for isolated test database
    - ensure MockMvc and environment properties are properly injected for tests

--- project/{{cookiecutter.project_name}}/bootstrap/app/src/test/groovy/{{cookiecutter.package_dir}}/app/AppBaseSpecification.groovy
+++ project/{{cookiecutter.project_name}}/bootstrap/app/src/test/groovy/{{cookiecutter.package_dir}}/app/AppBaseSpecification.groovy
@@ -3,9 +3,12 @@ package {{cookiecutter.package}}.app
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.env.Environment
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.servlet.MockMvc
+import org.testcontainers.containers.PostgreSQLContainer
 import spock.lang.Specification
 
 @ActiveProfiles("test")
@@ -13,14 +16,38 @@ import spock.lang.Specification
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
-@ContextConfiguration
 class AppBaseSpecification extends Specification {
 
-  @Autowired private MockMvc mvc
+    @Autowired
+    private MockMvc mvc
 
-  def 'contextLoads'() {
-      expect:
-      mvc != null
-  }
+    @Autowired
+    private Environment environment
+
+    private static postgres = new PostgreSQLContainer("postgres:16-alpine")
+
+    static {
+        postgres.start()
+    }
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    def 'contextLoads'() {
+        expect:
+        mvc != null
+    }
+
+    def 'database is running'() {
+        expect:
+        postgres.isRunning()
+        postgres.jdbcUrl == environment.getProperty('spring.datasource.url')
+        postgres.username == environment.getProperty('spring.datasource.username')
+        postgres.password == environment.getProperty('spring.datasource.password')
+    }
 
 }
