5bc3a157de53c40a77cfbac247c45d4d0df716e8
tzatzosm <marsel.tzatzos@gmail.com>
2025-08-02 21:47:33 +0300

    ✨ feat(build): add addFeature Gradle task and improve feature cookiecutter template
    - introduce addFeature Gradle task for automated feature module generation using cookiecutter
    - update feature cookiecutter.json for correct variable usage and improved template flexibility
    
    🐛 fix(feature): remove Hello example classes from feature template
    
    - delete HelloController, use cases, domain models, ports, and infrastructure example
    - ensure generated features start clean without sample code

--- feature/{{cookiecutter.feature_name}}/src/main/java/{{cookiecutter.base_package_dir}}/{{cookiecutter.feature_package}}/domain/model/Message.java
+++
@@ -1,7 +0,0 @@
-package {{cookiecutter.package}}.domain.model;
-
-import lombok.Builder;
-import lombok.NonNull;
-
-@Builder
-public record Message(@NonNull String value) { }
--- feature/{{cookiecutter.feature_name}}/src/main/java/{{cookiecutter.base_package_dir}}/{{cookiecutter.feature_package}}/domain/ports/HelloMessageService.java
+++
@@ -1,7 +0,0 @@
-package {{cookiecutter.package}}.domain.ports;
-
-public interface HelloMessageService {
-    String getHelloMessage();
-
-    String getHelloMessage(String name);
-}
